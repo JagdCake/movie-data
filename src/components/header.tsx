cff195db80989fd447f806edc657400d9dbbea72
Ivo Angelov <jc@jagdcake.com>
2020-05-07 16:58:26 +0300

    Style the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,14 +10,12 @@ interface HeaderProps {
 const Header: FunctionComponent<HeaderProps> = ({
     siteTitle,
 }: HeaderProps): ReactElement => (
-    <header className="bg-purple-800 mb-6">
-        <div className="mx-auto max-w-4xl py-6 px-4">
-            <h1 className="text-4xl">
-                <Link to="/" className="text-white">
-                    {siteTitle}
-                </Link>
-            </h1>
-        </div>
+    <header className="my-6">
+        <h1 className="text-3xl text-center">
+            <Link to="/" className="text-purple">
+                {siteTitle}
+            </Link>
+        </h1>
     </header>
 );
 

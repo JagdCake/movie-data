59ac8d0a96678252cc70b2d24c6a9186d4505092
Ivo Angelov <jc@jagdcake.com>
2020-05-08 14:27:53 +0300

    Create info card component
    Displays a square card which has an anchor link leading to itself.

--- src/components/info-card.tsx
+++ src/components/info-card.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import { ReactNode } from 'react';
+import { FunctionComponent } from 'react';
+import { ReactElement } from 'react';
 
 interface InfoCardProps {
     children: ReactNode;
     id: string;
 }
+
+const InfoCard: FunctionComponent<InfoCardProps> = ({
+    children,
+    id,
+}: InfoCardProps): ReactElement => {
+    return (
+        <article
+            id={id}
+            aria-label="Info card"
+            className="border-2 border-purple rounded-sm px-3 pb-6 text-lg"
+        >
+            <a href={`#${id}`} className="card-link"></a>
+            {children}
+        </article>
+    );
+};
+
+export default InfoCard;

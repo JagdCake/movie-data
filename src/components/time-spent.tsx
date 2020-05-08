b336e39f29038d3d6e0f0f4a5f3b8bb97515799b
Ivo Angelov <jc@jagdcake.com>
2020-05-08 16:41:00 +0300

    Create HTML time element component

--- src/components/time-spent.tsx
+++ src/components/time-spent.tsx
@@ -5,3 +5,16 @@ import { ReactElement } from 'react';
 interface TimeProp {
     date: string;
 }
+
+const Time: FunctionComponent<TimeProp> = ({
+    date,
+}: TimeProp): ReactElement => {
+    return (
+        <time
+            dateTime={date}
+            className="p-1 bg-purple text-gray rounded-sm"
+        >
+            {date}
+        </time>
+    );
+};

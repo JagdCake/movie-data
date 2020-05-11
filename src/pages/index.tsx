c9f0d455cd7d2c6e36cec8af651b3debfbecc9a6
Ivo Angelov <jc@jagdcake.com>
2020-05-11 09:55:53 +0300

    Display "time spent" info card using hard-coded data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,16 @@ import { ReactElement } from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import TimeSpent from '../components/time-spent';
 
 const IndexPage: FunctionComponent = (): ReactElement => (
     <Layout>
         <SEO title="Home" />
+        <TimeSpent
+            fromDate="Jun 2016"
+            untilDate="Dec 21 2019"
+            numberOfMoviesWatched="672"
+        />
     </Layout>
 );
 

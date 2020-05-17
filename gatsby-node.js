c5381c27401a1579ac2d354b129c209360fa2c7f
Ivo Angelov <jc@jagdcake.com>
2020-05-17 17:16:37 +0300

    Query for movie "watched_on" date ranges to generates pages from

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,26 @@
 require('ts-node').register({ files: true });
 
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions;
+
+    // 48 is also the number of movies per page on my movies website
+    const rangeOfMoviesToGenerateDataFrom = 48;
+
+    return graphql(
+        `
+            query MovieDateRanges($range: Int!) {
+                p: postgres {
+                    m: movieById(id: 1) {
+                        rangeOfMovieDates(range: $range)
+                    }
+                }
+            }
+        `,
+        { range: rangeOfMoviesToGenerateDataFrom }
+    ).then((result) => {
+        if (result.errors) {
+            throw result.errors;
+        }
+    });
+};

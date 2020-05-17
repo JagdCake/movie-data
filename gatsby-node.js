6d933fd380f242a32cbbdf1ffdc050b619498e6b
Ivo Angelov <jc@jagdcake.com>
2020-05-17 17:45:00 +0300

    Generate page paths using ISO formatted dates

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,56 @@
 require('ts-node').register({ files: true });
 const path = require('path');
 
+// copied from ./src/components/time-spent.tsx because you can't import
+// from ES6 modules in this file, in a straightforward way
+const isoMonth = (monthName) => {
+    // the month name format is the same as in the database
+    switch (monthName) {
+        case 'Jan':
+            return '01';
+        case 'Feb':
+            return '02';
+        case 'Mar':
+            return '03';
+        case 'Apr':
+            return '04';
+        case 'May':
+            return '05';
+        case 'Jun':
+            return '06';
+        case 'Jul':
+            return '07';
+        case 'Aug':
+            return '08';
+        case 'Sep':
+            return '09';
+        case 'Oct':
+            return '10';
+        case 'Nov':
+            return '11';
+        case 'Dec':
+            return '12';
+        default:
+            throw Error(
+                `The format of the month value "${monthName}" is incorrect.`
+            );
+    }
+};
+
+// copied from ./src/components/time-spent.tsx because you can't import
+// from ES6 modules in this file, in a straightforward way
+// function is meant to work only with dates that follow the format of
+// the "watched on" fields from the database
+const isoDate = (date) => {
+    const dayMonthYear = date.split(' ');
+
+    const year = dayMonthYear[2];
+    const monthName = dayMonthYear[1];
+    const month = isoMonth(monthName);
+    const day = dayMonthYear[0];
+
+    return `${year}-${month}-${day}`;
+};
 
 exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions;
@@ -37,7 +87,7 @@ exports.createPages = async ({ graphql, actions }) => {
                 const endRangeDate = movieWatchedOnDateRanges[j];
 
                 createPage({
-                    path: `${startRangeDate}_until_${endRangeDate}`,
+                    path: `${isoDate(startRangeDate)}_until_${isoDate(endRangeDate)}`,
                     component: path.resolve(`./src/templates/data-range.tsx`),
                     context: {
                         movieDateRange: [startRangeDate, endRangeDate],

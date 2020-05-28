d9d2e03b9bb119c0eb382c45d0d9567da264d676
Ivo Angelov <jc@jagdcake.com>
2020-05-28 16:16:45 +0300

    Create function to search through movie ranges for a value

--- src/components/movie-range-selector.tsx
+++ src/components/movie-range-selector.tsx
@@ -2,6 +2,32 @@ import React from 'react';
 import { FunctionComponent } from 'react';
 import { ReactElement } from 'react';
 
+const movieRangeSearch = (
+    movieRanges: string[],
+    searchValue: string
+): string[] => {
+    const regexZeroOrMoreOfAnything = '.*';
+
+    const searchValueInterspersedWithRegex = searchValue.replace(
+        ' ',
+        regexZeroOrMoreOfAnything
+    );
+
+    const searchTerm = RegExp(
+        regexZeroOrMoreOfAnything +
+            searchValueInterspersedWithRegex +
+            regexZeroOrMoreOfAnything,
+        'i'
+    );
+
+    return movieRanges.filter((range) => {
+        const searchTermFoundInRange = searchTerm.test(range);
+        if (searchTermFoundInRange) {
+            return range;
+        }
+    });
+};
+
 interface MovieRangeSelectorProps {
     listOfRanges: string[];
     movieRangeSearchValue: string;

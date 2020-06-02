d10ae6452682d73f01f348350308ef1937d00eb3
Ivo Angelov <jc@jagdcake.com>
2020-06-02 18:50:45 +0300

    Define props for a "movie runtime" section component

--- src/components/movie-length.tsx
+++ src/components/movie-length.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { FunctionComponent } from 'react';
 import { ReactElement } from 'react';
 
+interface MovieRuntimeSectionProps {
+    sectionTitle: 'Longest runtime' | 'Shortest runtime' | 'Average runtime';
+    movieTitle: string;
+    movieRuntimeHours: string;
+    movieRuntimeMinutes: string;
+}
+
+
 type MovieAndItsRuntime = [string, string];
 
 interface MovieLengthProps {

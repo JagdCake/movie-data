bc56bc1593ebe4989b38cc81f93b11f46edff8c6
Ivo Angelov <jc@jagdcake.com>
2020-06-08 11:53:24 +0300

    Create genre list component
    Displays number of movies per genre.

--- src/components/genre-list.tsx
+++ src/components/genre-list.tsx
@@ -2,7 +2,33 @@ import React from 'react';
 import { FunctionComponent } from 'react';
 import { ReactElement } from 'react';
 import InfoCard from './info-card';
+import ValuesAndCountsList from './values-and-counts-list';
 
 interface GenreListProp {
     list: [string, string][];
 }
+
+const GenreList: FunctionComponent<GenreListProp> = ({
+    list,
+}: GenreListProp): ReactElement => {
+    const topGenre = list[0][0];
+    const genreSummary: ReactElement = (
+        <p>
+            <span>I mostly watch </span>
+            <span className="highlight">{topGenre}</span>
+            <span> movies, but I&apos;m open to every genre.</span>
+        </p>
+    );
+
+    return (
+        <InfoCard id="movie-genres">
+            <ValuesAndCountsList
+                listSummary={genreSummary}
+                listHeading="Number of movies per genre"
+                list={list}
+            />
+        </InfoCard>
+    );
+};
+
+export default GenreList;

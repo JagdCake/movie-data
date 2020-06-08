1f68e9dbace51789164e96d5802bf9a644a553b0
Ivo Angelov <jc@jagdcake.com>
2020-06-08 11:44:35 +0300

    Create values and counts lists component
    Displays a list summary, heading describing the list, and the list itself which is made up of the emphasized value (on the left) and the count (on the right).

--- src/components/values-and-counts-list.tsx
+++ src/components/values-and-counts-list.tsx
@@ -7,3 +7,33 @@ interface ValuesAndCountsListProps {
     listHeading: string;
     list: [string, string][];
 }
+
+const ValuesAndCountsList: FunctionComponent<ValuesAndCountsListProps> = ({
+    listSummary,
+    listHeading,
+    list,
+}: ValuesAndCountsListProps): ReactElement => {
+    return (
+        <>
+            <div className="mb-4 pb-4 border-b-2 border-dashed">
+                {listSummary}
+            </div>
+            <section>
+                <h1 className="font-bold text-center">{listHeading}</h1>
+                <ul>
+                    {list.map(([value, count], index) => (
+                        <li
+                            key={value + index.toString()}
+                            className="py-2 flex justify-between border-b border-dotted"
+                        >
+                            <em>{value}</em>
+                            <strong>{count}</strong>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </>
+    );
+};
+
+export default ValuesAndCountsList;

a69966fe4e1953512460c84d8c4f50c3733afb9e
Ivo Angelov <jc@jagdcake.com>
2020-06-08 11:48:52 +0300

    Define prop for a genre list component

---
+++ src/components/genre-list.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FunctionComponent } from 'react';
+import { ReactElement } from 'react';
+import InfoCard from './info-card';
+
+interface GenreListProp {
+    list: [string, string][];
+}

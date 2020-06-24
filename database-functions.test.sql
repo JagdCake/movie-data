baf67df8e328170d444a6fcd5c49d83564b2df5e
Ivo Angelov <jc@jagdcake.com>
2020-06-24 15:31:26 +0300

    Test if IMDb ratings are correctly compared to mine

--- database-functions.test.sql
+++ database-functions.test.sql
@@ -169,3 +169,55 @@ BEGIN;
         raise info '%', success_message;
     END $$ LANGUAGE 'plpgsql';
 ROLLBACK;
+
+-- Test movie_imdb_ratings_score_compared_to_mine
+BEGIN;
+    drop trigger prevent_deleting_older_movies on movie;
+    -- delete all movies besides 1 which I've rated using a rating which
+    -- has a higher score than that of its translated IMDb rating
+    delete from movie
+    where id not in (
+        select id
+        from movie
+        where my_rating = 'Great Onion' -- score of 4
+        and imdb_rating < 6.0 -- translated to "Good Tomato" (at best), so score <= 3
+        limit 1
+    );
+
+    DO $$
+        declare got text;
+        declare expected text;
+        declare header_message text;
+        declare failure_message text;
+        declare success_message text;
+    BEGIN
+        select movie_imdb_ratings_score_compared_to_mine(
+            (select movie from movie limit 1),
+            ARRAY['01 Jun 2016', '31 Dec 2099']
+        ) into got;
+
+        select 'lower' into expected;
+
+        select
+        E'\nTEST movie_imdb_ratings_score_compared_to_mine '
+        into header_message;
+
+        select
+        header_message
+        || 'FAIL'
+        || E'\n\tGot: "'
+        || got
+        || E'"\n\tExpected: "'
+        || expected
+        || '"' into failure_message;
+
+        assert got = expected, failure_message;
+
+        select
+        header_message
+        || 'PASS'
+        into success_message;
+
+        raise info '%', success_message;
+    END $$ LANGUAGE 'plpgsql';
+ROLLBACK;
